1ad0ed2b426ef9cf6314d75c7319b5d4f47ea69a
liangyue2018 <1872546516@qq.com>
2025-10-22 15:55:53 +0800

    Sync

--- setup_oceanmesh2d.m
+++
@@ -1,7 +0,0 @@
-function setup_oceanmesh2d()
-% add paths for OceanMesh2D
-
-BasePath = fileparts(mfilename('fullpath'));
-
-addpath(genpath(fullfile(BasePath,'datasets')))
-addpath(genpath(fullfile(BasePath,'utilities')))
